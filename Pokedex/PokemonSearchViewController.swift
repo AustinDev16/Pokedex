31bc126a334bc0aeccb64f9701fa0d68268d0168
AustinDev16 <austin.blaser@gmail.com>
2016-08-30 12:02:01 -0600

    Added views in storyboard, searchViewController, connected outlets.

---
+++ Pokedex/PokemonSearchViewController.swift
@@ -0,0 +1,28 @@
+//
+//  PokemonSearchViewController.swift
+//  Pokedex
+//
+//  Created by Austin Blaser on 8/30/16.
+//  Copyright © 2016 Austin Blaser. All rights reserved.
+//
+
+import UIKit
+
+class PokemonSearchViewController: UIViewController {
+
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var idLabel: UILabel!
+    @IBOutlet weak var abilitiesLabel: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+  
+
+}
