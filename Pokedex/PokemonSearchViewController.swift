0ee4300a92bff9fed8b8ac373c476972e5abe84a
AustinDev16 <austin.blaser@gmail.com>
2016-08-30 12:22:59 -0600

    Set up view and finished project

--- Pokedex/PokemonSearchViewController.swift
+++ Pokedex/PokemonSearchViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class PokemonSearchViewController: UIViewController {
+class PokemonSearchViewController: UIViewController, UISearchBarDelegate {
 
+    
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var idLabel: UILabel!
@@ -20,7 +21,20 @@ class PokemonSearchViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        guard let searchTerm = searchBar.text else { return}
+        self.view.endEditing(true)
+        PokemonController.fetchPokemon(searchTerm) { (pokemon) in
+            
+         dispatch_async(dispatch_get_main_queue(), {
+                guard let pokemon = pokemon else {return}
+                self.nameLabel.text = pokemon.name.capitalizedString
+                self.idLabel.text = "ID: \(pokemon.id)"
+                self.abilitiesLabel.text = "Abilities: \(pokemon.abilities.joinWithSeparator(", "))"
+            })
+        }
     }
 
   
