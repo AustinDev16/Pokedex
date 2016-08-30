3e08f460f01ac3a4aa1187f1a5b6613a24fb61d6
AustinDev16 <austin.blaser@gmail.com>
2016-08-30 11:31:11 -0600

    Added Pokemon controller and tested that the network request is executing correctly and creating a Pokemon object.

--- Pokedex/ViewController.swift
+++ Pokedex/ViewController.swift
@@ -13,6 +13,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        PokemonController.fetchPokemon("pikachu") { (pokemon) in
+            dispatch_async(dispatch_get_main_queue(), { 
+                print("finished")
+                print(pokemon!.name)
+                
+            })
+        }
     }
 
     override func didReceiveMemoryWarning() {
